7a3c35947a6513b947df971bc4451a30711b30a2
Randall Williams <randallw1996@gmail.com>
2019-12-12 00:15:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.all? do |word|
-     word.size > 4
-  end
+  array.all? { |word| word.size > 4 }
 end
 
 def find_the_cheese# code an argument here
