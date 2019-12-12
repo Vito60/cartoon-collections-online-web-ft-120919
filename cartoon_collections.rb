a3b96eafa9d0f55032b4545d61a9e6ce9d8458ce
Randall Williams <randallw1996@gmail.com>
2019-12-12 00:32:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,4 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect{|cheese| cheese == cheese_types}
-  array.detect
 end
